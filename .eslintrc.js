26f6dc451f3b852762f152c848680c35b64531c5
Steven David Pillay <spillay6@myseneca.ca>
2024-08-03 22:00:56 -0400

    resolve merge conflict

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,28 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
+  "env": {
+    "commonjs": true,
+    "es2021": true,
+    "node": true,
+    // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+    "jest": true
   },
-  extends: 'eslint:recommended',
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
+  "extends": "eslint:recommended",
+  "overrides": [
+    {
+      "env": {
+        "node": true
+      },
+      "files": [
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      }
+    }
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest"
   },
-  rules: {},
-};
+  "rules": {
+  }
+}
